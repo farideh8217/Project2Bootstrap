5d1da0fd3773f1514b4c20486d01fcb15cfa2b20
farideh zaker <farideh.zaker8217@yahoo.com>
2022-03-06 18:47:26 +0330

    project dovom toplearn

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    $(window).scroll(function(){
+        if( $(this).scrollTop() > 50 ){
+            $('#main-menu').addClass('scroll');
+        }else{
+            $('#main-menu').removeClass('scroll');
+        }
+    })
+
+    $('#main-menu').on('show.bs.collapse', function () {
+        $('#main-menu').addClass('scroll-m');
+      })
+
+    $('#main-menu').on('hidden.bs.collapse', function () {
+        $('#main-menu').removeClass('scroll-m');
+      })
+
+
+
+})
